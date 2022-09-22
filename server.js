33ddc4ddfb438ad2eda2c80ac383cc93a70e3c4d
mochamad rafli <rafliforbusiness@gmail.com>
2022-09-23 00:36:25 +0700

    finish frontend and connect logic

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const network = require("node-network-manager");
-const { exec } = require('child_process');
+const { exec , spawnSync} = require('child_process');
 const wifi = require('node-wifi');
 const bodyParser = require('body-parser');
-
+const path = require( 'path' );
 const jsonParser = bodyParser.json()
 
 wifi.init({
@@ -13,8 +13,14 @@ wifi.init({
 
 const app = express()
 
+// endpoint untuk redirect ke view list wifi
+app.get('/', async(req, res) => {
+  res.sendFile(path.join(__dirname+'/views/index.html'));
+})
+
 app.get('/networks',jsonParser, async(req,res) =>{
     const networks = await wifi.scan()
+    console.log(networks)
     res.json(networks)
 });
 
@@ -27,10 +33,6 @@ app.get('/networks/connect',jsonParser, async(req,res) => {
       const ethernet = data.find((item) => item.TYPE === "ethernet");
       const device = data.find((item) => item.TYPE === "wifi");
       await network
-        .getConnectionProfilesList()
-        .then((data) => console.log(data))
-        .catch((error) => console.log(error));
-      await network
         .getDeviceInfoIPDetail(ethernet.DEVICE)
         .then((data) => networks.push(data))
         .catch((error) => console.log(error));
@@ -39,51 +41,43 @@ app.get('/networks/connect',jsonParser, async(req,res) => {
         .then((data) => networks.push(data))
         .catch((error) => console.log(error));
     })
-    exec(`nmap ${networks[0].gatewayV4}`, (err, stdout, stderr) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        if(stdout){
-            networks[0].nmap = stdout
-        }
+    if(networks.length >= 1){
+      const nmap = spawnSync('nmap',[networks[0].gatewayV4])
+      let dataNmap = nmap.stdout.toString().trim()
+      networks[0].nmap = dataNmap.split('\n')
+  
+      const loss = spawnSync('ping',['-c5',networks[0].gatewayV4])
+      let dataLoss = loss.stdout.toString().trim()
+      dataLoss = dataLoss.split('\n')[8].split(',')[2]
+      networks[0].loss = dataLoss
+    }
 
-        res.json(networks)
-      });
+    res.json(networks)
+
 });
+
+app.delete('/networks/connect',jsonParser,(req,res) => {
+  const ssid = req.body.ssid
+  console.log(req.body)
+  if(!ssid){
+    throw new Error('ssid is required')
+  }
+  const result = spawnSync('nmcli',["con", "down", "id", ssid])
+  res.json(result)
+
+})
 
 app.post('/networks/connect',jsonParser, async(req,res) => {
     const ssid = req.body.ssid
     const password = req.body.password
-    await wifi.connect({ ssid: ssid, password: password });
+    console.log(req.body)
+    try{
+      await wifi.connect({ ssid: ssid, password: password });
+    }catch(e){
+      console.log(e)
+    }
+    res.json({status:"success"})
       
-    const networks = []
-    await network
-    .getConnectionProfilesList(false)
-    .then(async (data) => {
-      const ethernet = data.find((item) => item.TYPE === "ethernet");
-      const device = data.find((item) => item.TYPE === "wifi");
-      await network
-        .getDeviceInfoIPDetail(ethernet.DEVICE)
-        .then((data) => networks.push(data))
-        .catch((error) => console.log(error));
-      await network
-        .getDeviceInfoIPDetail(device.DEVICE)
-        .then((data) => networks.push(data))
-        .catch((error) => console.log(error));
-    })
-
-    exec(`nmap ${networks[0].gatewayV4}`, (err, stdout, stderr) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        if(stdout){
-            networks[0].nmap = stdout
-        }
-
-        res.json(networks)
-      });
 });
 
 app.post('')
