59329fec7ac6ad39c86d982401aa3333da7b21ae
mochamad rafli <rafliforbusiness@gmail.com>
2022-09-24 15:26:05 +0700

    add network scan feature

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const network = require("node-network-manager");
-const {spawnSync} = require('child_process');
+const {spawnSync, exec} = require('child_process');
 const wifi = require('node-wifi');
 const bodyParser = require('body-parser');
 const path = require( 'path' );
+const fs = require("fs");
 const jsonParser = bodyParser.json()
+
 
 wifi.init({
     iface: null // network interface, choose a random wifi interface if set to null
@@ -13,15 +15,84 @@ wifi.init({
 
 const app = express()
 
+const getListInterface = () => {
+  let listIntervace = spawnSync('iw', ['dev'])
+  listIntervace = listIntervace.stdout.toString().trim().split('\n')
+  let interface =""
+  for(let i = 0; i < listIntervace.length; i++){
+    if(listIntervace[i].split(' ')[0].match("\tInterface")){
+      interface = listIntervace[i].split(' ')[1]
+      break
+    }
+  }
+
+  return interface
+}
 // endpoint untuk redirect ke view list wifi
 app.get('/', async(req, res) => {
   res.sendFile(path.join(__dirname+'/views/index.html'));
+})
+
+// endpoint untuk redirect ke view list traffic
+app.get('/traffic', async(req, res) => {
+  res.sendFile(path.join(__dirname+'/views/traffic.html'));
 })
 
 app.get('/networks',jsonParser, async(req,res) =>{
     const networks = await wifi.scan()
     res.json(networks)
 });
+
+app.get('/networks/monitor/on', jsonParser, async(req, res) => {
+  // mencari nama wifi card
+  const interface = getListInterface()
+  
+  // connect dengan service airmon-ng
+  exec(`echo calonsukses | sudo -S airmon-ng start ${interface}`)
+
+  res.json(interface)
+})
+
+app.get('/networks/monitor/scan', jsonParser, async(req, res) => {
+  const interface = getListInterface()
+
+  await exec(`echo "calonsukses" | sudo -S timeout 5s airodump-ng -w scan/output ${interface} --output-format csv`,{ maxBuffer: 1024 * 10000000 }, (err, stdout, stderr) => {
+    if (err) {
+      console.error(`exec error: ${err}`);
+      return;
+    }
+  });
+
+  res.json("success")
+})
+
+app.get('/networks/monitor/result', jsonParser, (req, res) => {
+  const ls = spawnSync("ls", ["scan"])
+  files = ls.stdout.toString().trim().split("\n")[0]
+  output = ''
+  csv = fs.readFileSync(path.join(__dirname+`/scan/${files}`)).toString()
+  if(csv){
+    output += csv.toString().trim()
+  }else{
+    output += "traffic kosong"
+  }
+  // for(let i = files.length-1; i >= 0;i--){
+  //   csv = fs.readFileSync(path.join(__dirname+`/scan/${files[i]}`)).toString()
+  //   if(csv){
+  //     output += csv.toString().trim()
+  //   }
+  // }
+  res.json(output.split("\n"))
+})
+app.get('/networks/monitor/off', jsonParser, async(req, res) => {
+
+  const interface = getListInterface()
+
+  // menghentikan service airmon
+  exec(`echo calonsukses |sudo airmon-ng stop ${interface}`)
+  res.json(interface)
+
+})
 
 app.get('/networks/connect',jsonParser, async(req,res) => {
     let networks = []
